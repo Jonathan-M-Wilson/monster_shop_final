bcdfd699f7ff2b9a8d94cb88330ca1d673882afa
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-08-01 13:16:32 -0600

    Add new and create methods to Merchant Coupons Controller

--- app/controllers/merchant/coupons_controller.rb
+++ app/controllers/merchant/coupons_controller.rb
@@ -4,6 +4,20 @@ class Merchant::CouponsController < Merchant::BaseController
     @coupons = merchant.coupons
   end
 
+  def new
+    @coupon = Coupon.new
+  end
+
+  def create
+    @coupon = current_user.merchant.coupons.new(coupon_params)
+    if @coupon.save
+      redirect_to '/merchant/coupons'
+    else
+      flash[:error] = @coupon.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
   def update
     @coupon = Coupon.find(params[:id])
     if params[:coupon]
