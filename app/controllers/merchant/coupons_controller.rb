89c3e367fba0107b9b2ded7563a477cd1e7b4d19
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-08-01 12:04:15 -0600

    Create Merchant Coupons Controller and add index and update methods, with private param methods

---
+++ app/controllers/merchant/coupons_controller.rb
@@ -0,0 +1,36 @@
+class Merchant::CouponsController < Merchant::BaseController
+  def index
+    merchant = Merchant.find(current_user.merchant.id)
+    @coupons = merchant.coupons
+  end
+
+  def update
+    @coupon = Coupon.find(params[:id])
+    if params[:coupon]
+      @coupon.update(coupon_params)
+      if @coupon.save
+        redirect_to '/merchant/coupons'
+      else
+        flash[:error] = @coupon.errors.full_messages.to_sentence
+        render :edit
+      end
+    elsif params[:enabled]
+      @coupon.update(enabled_param)
+      if @coupon.enabled
+        flash[:success] = "This coupon is now Enabled"
+      else
+        flash[:success] = "This coupon is now Disabled"
+      end
+      redirect_to '/merchant/coupons'
+    end
+  end
+
+  private
+  def coupon_params
+    params.require('coupon').permit(:name, :code, :percent_off)
+  end
+
+  def enabled_param
+    params.permit(:enabled)
+  end
+end
