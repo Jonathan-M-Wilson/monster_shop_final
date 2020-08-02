6bbc88217a99bb1d1af336553163710b04f965a4
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-08-02 09:07:37 -0600

    Create spec for the coupon show page

---
+++ spec/features/merchant/coupons/show_spec.rb
@@ -0,0 +1,67 @@
+require 'rails_helper'
+
+RSpec.describe 'As a logged-in merchant user', type: :feature do
+  before(:each) do
+    @meg = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
+
+    @merchant_user = User.create!(name: "Jose",
+                                  address: "789 Jkl St.",
+                                  city: "Denver",
+                                  state: "Colorado",
+                                  zip: "80202",
+                                  email: "merchant@hotmail.com",
+                                  password: "mer",
+                                  role: 1,
+                                  merchant_id: @meg.id)
+
+    @coupon_1 = Coupon.create!(name: "4th of July 30%-Off", code: "FREEDOM", percent_off: 30, merchant: @meg)
+    @coupon_2 = Coupon.create!(name: "Holiday Weekend 50%-Off", code: "TAKE50", percent_off: 50, merchant: @meg)
+    @coupon_3 = Coupon.create!(name: "Bulk 5%-Off", code: "BULK", percent_off: 5, merchant: @meg, enabled: false)
+
+    visit root_path
+    click_on 'Log In'
+
+    fill_in 'Email', with: @merchant_user.email
+    fill_in 'Password', with: @merchant_user.password
+
+    click_button 'Log In'
+  end
+
+  describe 'When I visit my coupons index' do
+    before(:each) do
+      visit '/merchant/coupons'
+    end
+
+    it "should show both the id and name as links to that coupon's show page" do
+      expect(page).to have_link("#{@coupon_1.id}", href: "/merchant/coupons/#{@coupon_1.id}")
+      expect(page).to have_link("#{@coupon_1.name}", href: "/merchant/coupons/#{@coupon_1.id}")
+      expect(page).to have_link("#{@coupon_2.id}", href: "/merchant/coupons/#{@coupon_2.id}")
+      expect(page).to have_link("#{@coupon_2.name}", href: "/merchant/coupons/#{@coupon_2.id}")
+      expect(page).to have_link("#{@coupon_3.id}", href: "/merchant/coupons/#{@coupon_3.id}")
+      expect(page).to have_link("#{@coupon_3.name}", href: "/merchant/coupons/#{@coupon_3.id}")
+    end
+
+    describe 'When I click on that ID or name for a coupon' do
+      before(:each) do
+        click_on @coupon_1.name
+      end
+
+      it 'I\'m taken to this coupon\'s show page"' do
+        expect(current_path).to eq("/merchant/coupons/#{@coupon_1.id}")
+      end
+
+      describe "Once I am on the coupon's show page" do
+        it " see that coupon's id, name, code, and percent_off" do
+          expect(page).to have_content("#{@coupon_1.name} Information")
+          expect(page).to have_content("ID: #{@coupon_1.id}")
+          expect(page).to have_content("Name: #{@coupon_1.name}")
+          expect(page).to have_content("Code: #{@coupon_1.code}")
+          expect(page).to have_content("Status: #{@coupon_1.enabled?}")
+          expect(page).to have_content("Percent Off: -#{@coupon_1.percent_off}%")
+          expect(page).to have_content("Created: #{@coupon_1.created_at}")
+          expect(page).to have_content("Last Updated: #{@coupon_1.updated_at}")
+        end
+      end
+    end
+  end
+end
